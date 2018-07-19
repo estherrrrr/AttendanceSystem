7a2f076a2c57c22c9e473dd09ebeaf0edbeee628
czyc <qnzxxzqn@163.com>
2018-07-19 14:26:16 +0800

    Base

---
+++ src/main/java/com/etc/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.etc.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping({"/","login.html"})
+	public String gotoIndex(){
+		return "login";
+	}
+	
+	@RequestMapping("/login")
+	public String login(@RequestParam(required=true)String adminname,
+			@RequestParam(required=true)String password,HttpServletRequest request){
+		return null;
+		
+	}
+	
+	@RequestMapping("logout")
+	public String logout(HttpServletRequest request){
+		request.setAttribute("msg", "您已安全推出系统！");
+		request.getSession().invalidate();
+		return "login";
+	}
+
+}
