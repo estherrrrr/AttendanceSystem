488d3c58e4974432e88a147d414c446aaddc68f2
luoxudan <34167281+luoxudan@users.noreply.github.com>
2018-07-20 21:36:27 +0800

    jj

--- src/main/java/com/etc/controller/LoginController.java
+++ src/main/java/com/etc/controller/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.etc.entity.Admin;
+
 @Controller
 public class LoginController {
 	
@@ -21,7 +23,9 @@ public class LoginController {
 	@RequestMapping("/login")
 	public String login(@RequestParam(required=true)String adminname,
 			@RequestParam(required=true)String password,HttpServletRequest request){
-		request.getSession().setAttribute("admin", "admin");
+		Admin admin=new Admin();
+		admin.setUsername(adminname);
+		request.getSession().setAttribute("admin", admin);
 		return "adminMain";
 		
 	}
