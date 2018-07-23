2acf161deb68725a80c300868a7a0dfa65e4d7a8
estherrrrr <qnzxxzqn@163.com>
2018-07-23 16:58:12 +0800

    LoginController

--- src/main/java/com/etc/controller/LoginController.java
+++ src/main/java/com/etc/controller/LoginController.java
@@ -11,14 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.etc.dao.AdminMapper;
-import com.etc.entity.Admin;
-import com.etc.entity.AdminExample;
+import com.etc.dao.StudentMapper;
+import com.etc.dao.TeacherMapper;
+import com.etc.entity.*;
 
 @Controller
 public class LoginController {
 	
 	@Autowired
 	private AdminMapper adminMapper;
+	@Autowired
+	private TeacherMapper teacherMapper;
+	@Autowired
+	private StudentMapper studentMapper;
 	
 	@GetMapping({"/","login.html"})
 	public String gotoIndex(){
@@ -27,17 +32,44 @@ public class LoginController {
 	
 	@RequestMapping("/login")
 	public String login(@RequestParam(required=true)String adminname,
-			@RequestParam(required=true)String password,HttpServletRequest request){
-		AdminExample ae = new AdminExample();
-		ae.createCriteria().andUsernameEqualTo(adminname).andPwdEqualTo(password);
-		List<Admin> admins = adminMapper.selectByExample(ae);
-		if(admins.size()!=1){
-			request.setAttribute("msg", "您输入的用户名或密码错误！");
-			return "login";
-		}else{
-			request.getSession().setAttribute("admin", admins.get(0));
-			return "adminMain";
+			@RequestParam(required=true)String password,@RequestParam(required=true)String sex,HttpServletRequest request){
+
+		if(sex.equals("manager")){
+			AdminExample ae = new AdminExample();
+			ae.createCriteria().andUsernameEqualTo(adminname).andPwdEqualTo(password);
+			List<Admin> admins = adminMapper.selectByExample(ae);
+			if(admins.size()!=1){
+				request.setAttribute("msg", "您输入的用户名或密码错误！");
+				return "login";
+			}else{
+				request.getSession().setAttribute("admin", admins.get(0));
+				return "test";
+			}
 		}
+		else if(sex.equals("teacher")){
+			TeacherExample te=new TeacherExample();
+			te.createCriteria().andTnameEqualTo(adminname).andPwdEqualTo(password);
+			List<Teacher> teachers = teacherMapper.selectByExample(te);
+			if(teachers.size()!=1){
+				request.setAttribute("msg", "您输入的用户名或密码错误！");
+				return "login";
+			}else{
+				request.getSession().setAttribute("admin", teachers.get(0));
+				return "teacherMain";
+			}
+		}else{
+			StudentExample se=new StudentExample();
+			se.createCriteria().andSnameEqualTo(adminname).andPwdEqualTo(password);
+			List<Student> students = studentMapper.selectByExample(se);
+			if(students.size()!=1){
+				request.setAttribute("msg", "您输入的用户名或密码错误！");
+				return "login";
+			}else{
+				request.getSession().setAttribute("admin", students.get(0));
+				return "studentMain";
+			}
+		}
+		
 		
 	}
 	
@@ -48,4 +80,10 @@ public class LoginController {
 		return "login";
 	}
 
+	@RequestMapping("/academy")
+	public String showMyAca(@RequestParam String academy,HttpServletRequest request){
+		
+		request.getSession().setAttribute("academy", academy);
+		return "academyDetail";
+	}
 }
