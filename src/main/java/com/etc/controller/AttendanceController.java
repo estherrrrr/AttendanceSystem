69ef66a9b5bd3735484b7eb53f6f95ffd4679124
estherrrrr <qnzxxzqn@163.com>
2018-07-23 16:57:18 +0800

    update attendance pages

--- src/main/java/com/etc/controller/AttendanceController.java
+++ src/main/java/com/etc/controller/AttendanceController.java
@@ -1,9 +1,14 @@
 package com.etc.controller;
 
+import java.io.Console;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,5 +42,39 @@ public class AttendanceController {
 		map.add(l1);map.add(l2);map.add(l3);
 		JsonResult<List<List<Double>>> list=new JsonResult<List<List<Double>>>(map);
 		return list;
+	}
+	
+	
+	
+	@GetMapping("/showacademydetail")
+	public JsonResult<List<Double>> showAcademyDetail(HttpServletRequest request){
+		
+		String academy =(String) request.getSession().getAttribute("academy"); 
+		
+		
+		List<Double> map=new ArrayList<Double>();
+		map.add(97.0);map.add(93.8);
+		map.add(87.0);map.add(100.0);
+		JsonResult<List<Double>> list=new JsonResult<List<Double>>(map);
+		return list;
+		
+	}
+	
+	@GetMapping("/showtopclass")
+	public JsonResult<List<Map<String,Object>>> showTopClass(){
+		
+		Map<String,Object> map1=new HashMap<String,Object>();
+		map1.put("name", "社交");map1.put("num", 79.2);
+		Map<String,Object> map2=new HashMap<String,Object>();
+		map2.put("name", "计网");map2.put("num", 88.2);
+		Map<String,Object> map3=new HashMap<String,Object>();
+		map3.put("name", "计组");map3.put("num", 89.2);
+		Map<String,Object> map4=new HashMap<String,Object>();
+		map4.put("name", "面向对象");map4.put("num", 93.2);
+		Map<String,Object> map5=new HashMap<String,Object>();
+		map5.put("name", "数据库");map5.put("num", 97.2);
+		List<Map<String,Object>> list=new ArrayList<Map<String,Object>>();
+		list.add(map1);list.add(map2);list.add(map3);list.add(map4);list.add(map5);
+		return new JsonResult<List<Map<String,Object>>>(list);
 	}
 }
