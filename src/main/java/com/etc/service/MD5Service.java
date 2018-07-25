232cd6a5512e31130a0186d0821e56a3be5d0f62
lucity <34018426+lucity@users.noreply.github.com>
2018-07-25 21:17:12 +0800

    MD5加密

---
+++ src/main/java/com/etc/service/MD5Service.java
@@ -0,0 +1,28 @@
+package com.etc.service;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MD5Service {
+	 public static String EncoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+	        //确定计算方法
+	        MessageDigest md5=MessageDigest.getInstance("MD5");
+	        BASE64Encoder base64en = new BASE64Encoder();
+	        //加密后的字符串
+	        String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
+	        return newstr;
+	 }
+	 public static boolean checkPassword(String newpasswd,String oldpasswd) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+	        if(EncoderByMd5(newpasswd).equals(oldpasswd))
+	            return true;
+	        else
+	            return false;
+	    }
+
+}
